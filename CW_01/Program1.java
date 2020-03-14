ee0ee4b8f290f70b50615215c34ae4b27b905e79
Piotr Wasilewski <wasilewski.piotr0@gmail.com>
2020-03-14 14:53:22 +0100

    Create Program1.java

---
+++ CW_01/Program1.java
@@ -0,0 +1,20 @@
+package CW_01;
+
+import java.util.Scanner;
+
+public class Program1{
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+
+        boolean bool = true;
+        int x = 25;
+        double y = 5.43;
+        char c = 'h';
+
+        Scanner scan = new Scanner(System.in);
+        int StudentID = scan.nextInt();
+
+        System.out.println("Hello S"+StudentID);
+    }
+}
