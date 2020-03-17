8e430b34bc13d15d7f2c0593c208aca2ad7c8533
Piotr Wasilewski <wasilewski.piotr0@gmail.com>
2020-03-17 17:59:56 +0100

    Create Zadanie_5.java

---
+++ CW_05/Zadanie_5.java
@@ -0,0 +1,13 @@
+package CW_05;
+
+public class Zadanie_5{
+    public static void main(String[] args){
+
+        for(int x = -50; x<=50; x++){
+            if(x%2 == 0 && x%3 == 0){
+                System.out.println(x);
+            }
+        }
+
+    }
+}
